346491a0ccc50a73b2b92483c889bc605a5b8465
Alex Gusev <alex@flancer64.com>
2025-03-04 13:44:35 +0200

    SVTR-215 Add any payment processor to the site

--- src/Front/Defaults.js
+++ src/Front/Defaults.js
@@ -1,16 +1,16 @@
 /**
  * Plugin constants (hardcoded configuration) for frontend code.
  */
-export default class Vendor_Plugin_Front_Defaults {
-    /** @type {Vendor_Plugin_Shared_Defaults} */
+export default class Fl64_Paypal_Front_Defaults {
+    /** @type {Fl64_Paypal_Shared_Defaults} */
     SHARED;
 
     /**
-     * @param {Vendor_Plugin_Shared_Defaults} SHARED
+     * @param {Fl64_Paypal_Shared_Defaults} SHARED
      */
     constructor(
         {
-            Vendor_Plugin_Shared_Defaults$: SHARED,
+            Fl64_Paypal_Shared_Defaults$: SHARED,
         }
     ) {
         this.SHARED = SHARED;
--- src/Shared/Defaults.js
+++ src/Shared/Defaults.js
@@ -1,9 +1,9 @@
 /**
  * Plugin constants (hardcoded configuration) for shared code.
  */
-export default class Vendor_Plugin_Shared_Defaults {
+export default class Fl64_Paypal_Shared_Defaults {
 
-    NAME = '@flancer64/teq-agave-plugin';
+    NAME = '@flancer64/teq-agave-paypal';
 
 
     constructor() {
