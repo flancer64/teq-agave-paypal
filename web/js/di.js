0e80915e7d4e0aab57adb2d0e864f045aadb753d
Alex Gusev <alex@flancer64.com>
2025-03-17 20:50:51 +0200

    SVTR-241 Create an initial content for Pricing page

--- web/js/di.js
+++ web/js/di.js
@@ -8,8 +8,9 @@ const container = new Container();
 
 // Configure the resolver for handling dependencies by namespace.
 const resolver = container.getResolver();
-resolver.addNamespaceRoot('Fl64_Paypal_', '/src/@flancer64/teq-agave-paypal'); // Plugin namespace
-resolver.addNamespaceRoot('TeqFw_Core_', '/src/@teqfw/core', 'mjs'); // Core plugin namespace
-resolver.addNamespaceRoot('TeqFw_Web_', '/src/@teqfw/web', 'mjs'); // Web plugin namespace
+resolver.addNamespaceRoot('Fl64_Paypal_', '/src/@flancer64/teq-agave-paypal');
+resolver.addNamespaceRoot('TeqFw_Core_', '/src/@teqfw/core', 'mjs');
+resolver.addNamespaceRoot('TeqFw_Di_', '/src/@teqfw/di', 'js');
+resolver.addNamespaceRoot('TeqFw_Web_', '/src/@teqfw/web', 'mjs');
 
 export default container;
